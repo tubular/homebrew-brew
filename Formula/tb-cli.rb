370fb05ea1766e65fb8e433a62b9475fe769bcc6
Christian Verkerk <christian@tubularlabs.com>
2018-05-22 07:40:37 -0700

    IMP fix url endpoint

--- Formula/tb-cli.rb
+++ Formula/tb-cli.rb
@@ -1,7 +1,7 @@
 class TbCli < Formula
   desc "A general purpose CLI for Tubular"
   homepage "https://app.tubularlabs.com/"
-  url "http://archive.tubularlabs.com/brew/tbcli-0.0.1.tar.gz"
+  url "http://archive.tubularlabs.net/brew/tbcli-0.0.1.tar.gz"
   sha256 "85cc828a96735bdafcf29eb6291ca91bac846579bcef7308536e0c875d6c81d7"
 
   depends_on "kapacitor"
