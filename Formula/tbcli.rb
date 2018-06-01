14e596b527dcee95a48a46a9c87d35906c3fcace
Christian Verkerk <christian@tubularlabs.com>
2018-06-01 13:24:24 -0700

    IMP bump version

--- Formula/tbcli.rb
+++ Formula/tbcli.rb
@@ -1,15 +1,15 @@
 class TbCli < Formula
   desc "A general purpose CLI for Tubular"
   homepage "https://app.tubularlabs.com/"
-  url "http://archive.tubularlabs.net/brew/tbcli-0.0.1.tar.gz"
-  sha256 "5f4de641da5282620510a189a0084fe1c889c1490a79694282c85e65b855bc4b"
+  url "http://archive.tubularlabs.net/brew/tbcli-0.0.2.tar.gz"
+  sha256 "36735ed2bf5eb8fddfcc7f188371113cd8a1479389308c6edd0664a2d9adc746"
 
   depends_on "kapacitor"
   depends_on "python@3"
 
   # brew unpacks the tar.gz resulting in a tbcli.pex file which can be called directly
   def install
-    bin.install "kappa.pex" => "tb"
+    bin.install "tbcli.pex" => "tb"
   end
 
   test do
