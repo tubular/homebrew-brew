25a218d5a64ea866759b57006a602351d2ff4e6c
Oleksandr Kozlenko <oleksandr@tubularlabs.com>
2018-08-31 12:12:08 +0300

    IMP update tbcli to latest

--- Formula/tbcli.rb
+++ Formula/tbcli.rb
@@ -1,7 +1,7 @@
 class Tbcli < Formula
   desc "A general purpose CLI for Tubular"
   homepage "https://app.tubularlabs.com/"
-  url "http://archive.tubularlabs.net/brew/tbcli-0.0.3.tar.gz"
+  url "http://archive.tubularlabs.net/brew/tbcli-0.0.7.tar.gz"
   sha256 "91830a0a647bc3c4ffce4eed8ca540247d02403b018c4de6418f57c6e3ce9646"
 
   depends_on "kapacitor"
