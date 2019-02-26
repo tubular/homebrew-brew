020141b223c24d12ab0f9d5cf68fc948b7346c53
Christian Verkerk <christian@tubularlabs.com>
2019-02-25 21:06:46 -0800

    Update tbcli.rb

--- Formula/tbcli.rb
+++ Formula/tbcli.rb
@@ -1,7 +1,7 @@
 class Tbcli < Formula
   desc "A general purpose CLI for Tubular"
   homepage "https://app.tubularlabs.com/"
-  url "http://archive.tubularlabs.net/brew/tbcli-0.4.5.tar.gz"
+  url "http://archive.tubularlabs.net/brew/tbcli-0.4.8.tar.gz"
   sha256 "361c2dffaf743189f085287c6b210677ec288d8bb5ed34e5d1e2476ace06fb23"
 
   depends_on "kapacitor"
