c3d80784759b7295091fed85a1256f52b3f7336a
Vitalii Vokhmin <vitalii@tubularlabs.com>
2019-08-19 18:18:37 +0300

    Release tbcli 0.8.0

--- Formula/tbcli.rb
+++ Formula/tbcli.rb
@@ -1,11 +1,11 @@
 class Tbcli < Formula
   desc "A general purpose CLI for Tubular"
   homepage "https://app.tubularlabs.com/"
-  url "http://archive.tubularlabs.net/brew/tbcli-0.7.4.tar.gz"
-  sha256 "2e3d45bdaaee576fa761b151148e966cceaeb20fb990b072e4ca8ef6e8a6961c"
+  url "http://archive.tubularlabs.net/brew/tbcli-0.8.0.tar.gz"
+  sha256 "fbf7cb8b2d3ab44fd9317e3c1f568b4fca8868405980ccd023e7cb4275964b90"
 
   depends_on "kapacitor"
-  depends_on "awscli" 
+  depends_on "awscli"
   depends_on "jansson"
   depends_on "lz4"
   depends_on "avro-tools"
