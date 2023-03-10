99e185e6ea987fd976ac948928e75caeb0233188
ahryniv <ahryniv@tubularlabs.com>
2023-03-10 15:42:21 +0200

    Update tbcli version to 0.56.1, remove curl-openssl dep

--- Formula/tbcli.rb
+++ Formula/tbcli.rb
@@ -1,14 +1,13 @@
 class Tbcli < Formula
   desc "General purpose CLI for Tubular"
   homepage "https://app.tubularlabs.com/"
-  url "http://archive.tubularlabs.net/brew/tbcli-0.54.0.tar.gz"
-  sha256 "892426d38c6bc5761d708438518018f4edaf699b6b633f72b82aebb1228491f9"
+  url "http://archive.tubularlabs.net/brew/tbcli-0.56.1.tar.gz"
+  sha256 "f89e674183a8c4cbde45ad79641373688f0b01942d093d081c53f8a44ad29a2a"
 
   depends_on "avro-tools"
   depends_on "awscli"
   depends_on "coreutils"
   depends_on "curl"
-  depends_on "curl-openssl"
   depends_on "jansson"
   depends_on "kapacitor"
   depends_on "libxmlsec1"
