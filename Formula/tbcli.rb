e39b734348ddea7b8a0a738ab2b4185d41c4dc78
vova <vova@MacBook-Pro.local>
2023-06-26 14:38:22 +0300

    OPS-5072 publish new tbcli version

--- Formula/tbcli.rb
+++ Formula/tbcli.rb
@@ -2,7 +2,7 @@ class Tbcli < Formula
   desc "General purpose CLI for Tubular"
   homepage "https://app.tubularlabs.com/"
   url "http://archive.tubularlabs.net/brew/tbcli-1.0.0.tar.gz"
-  sha256 "d400061a4bcb5865062a84bf16c3f5c782fbd1f6160fdb601bc09bad551d0e77"
+  sha256 "fe3d95ee4525d51806ed1ea22d627bb61dae899b8b31d12f87ce1d987a5d3d47"
 
   depends_on "avro-tools"
   depends_on "awscli"
