3e18c8dfb8508e8a32d2e152e94f9f03f894c56c
Yurii Vlasenko <yvlasenko@users.noreply.github.com>
2023-08-18 10:04:17 +0300

    bump tbcli

--- Formula/tbcli.rb
+++ Formula/tbcli.rb
@@ -1,8 +1,8 @@
 class Tbcli < Formula
   desc "General purpose CLI for Tubular"
   homepage "https://app.tubularlabs.com/"
-  url "http://archive.tubularlabs.net/brew/tbcli-1.3.0.tar.gz"
-  sha256 "8893183cc68ab34fc77393b3657af0172bbc679fa7f3df7d1efb236b59314220"
+  url "http://archive.tubularlabs.net/brew/tbcli-1.3.1.tar.gz"
+  sha256 "4115ceefcbcd3f0d31e12bc781a8d3789cf7712744cf70953826abf6d512d321"
 
   depends_on "avro-tools"
   depends_on "awscli"
