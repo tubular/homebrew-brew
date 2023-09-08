74454d7fab2763634b28c2f0bcc1ea00456203af
vova <vova@MacBook-Pro.local>
2023-09-08 17:42:06 +0300

    IMP update tbcli to 1.6.0

--- Formula/tbcli.rb
+++ Formula/tbcli.rb
@@ -2,7 +2,7 @@ class Tbcli < Formula
   desc "General purpose CLI for Tubular"
   homepage "https://app.tubularlabs.com/"
   url "http://archive.tubularlabs.net/brew/tbcli-1.6.0.tar.gz"
-  sha256 "01fa7f1624270ce8e4be88767c724a00e5fda54238d0754fb1d224b0b7df5e4f"
+  sha256 "68905f4abd38e4c43741e0600a89b560ad61336f468524edfdc9f55b02013aa0"
 
   depends_on "avro-tools"
   depends_on "awscli"
